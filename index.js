6e0d2a3c280b30d7582b590d7ed07af21096a7c0
Joshua Winningham <joshua.winningham@gmail.com>
2023-01-21 08:51:47 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+  alert('I was clicked!');
+  input.addEventListener('click', addingEventListener);
 }
+
+input.addEventListener('click', addingEventListener);
+
